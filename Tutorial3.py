fd15fd5f0f4d6c07cbc3ca166e8684af953b28e3
malusigumede <melucgumede@gmail.com>
2015-04-29 23:01:56 +0200

    Update Tutorial3.py

--- Tutorial3.py
+++ Tutorial3.py
@@ -25,4 +25,41 @@ if __name__=='__main__':
     plt.show() 
     
     #Question2
+    def mycorr(x,y):    
+    ft1=fft(x)
+    ft2=fft(y)
+    ft2conj=np.conj(ft2)
+    return np.real(ifft(ft1*ft2conj))
+
+if __name__=='__main__':
+    x=np.arange(-10,10,0.1)
+    y=np.exp(-0.5*(x+5)**2/1.5**2)
+    ycorr=mycorr(y,y)
+    plt.plot(x,ycorr,'r')
+    plt.show()
     
+    #Question3
+    def myshift(x,n=0):
+    N=x.size
+    vec=np.zeros(N)
+    vec[n]=1
+    vecft=fft(vec)
+    ft1=fft(x)
+    return np.real(ifft(ft1*vecft))
+
+def mycorr(x,y):
+    ft1=fft(x)
+    ft2=fft(y)
+    ft2conj=np.conj(ft2)
+    return np.real(ifft(ft1*ft2conj))
+
+if __name__=='__main__':
+    x=np.arange(-10,10,0.1)
+    y=np.exp(-0.5*(x+5)**2/1.5**2)
+    ycorr=mycorr(y,y)
+    M=y.size
+    yshift=myshift(y,M/3)
+    yshiftcorr=mycorr(yshift,yshift)
+    plt.plot(x,ycorr)
+    plt.plot(x,yshiftcorr,'k')
+    plt.show()
